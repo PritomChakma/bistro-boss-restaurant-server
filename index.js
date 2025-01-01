425e9a9c9e7ec2370a7c3b70a7ed1a4efd1fe4fd
pritom <chakmapritom1@gmail.com>
2025-01-01 23:48:43 +0600

    added a cart collection

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
@@ -38,6 +39,12 @@ async function run() {
         res.send(result);
     })
 
+    // Cart collection
+    app.post("/carts", async(req, res)=>{
+      const cartItems =req.body
+      const result = await cartCollection.insertOne(cartItems)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
